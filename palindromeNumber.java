d2b88ba9be210d86b653b3947c476e27448536fe
NIRANJANI <niranjniranjani4@gmail.com>
2025-06-12 22:04:10 +0530

    Add files via upload

---
+++ palindromeNumber.java
@@ -0,0 +1,29 @@
+import java.util.Scanner; 
+
+public class palindromeNumber {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+
+        int original = num;
+        int reversed = 0;
+
+        if(num<0){
+            System.out.println(num+"not a palindrome number");
+        }
+        else{
+            if(num>0){
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num = num/10;
+            }
+        }
+        if(original == reversed){
+            System.out.println(original+"not a palindrome number");
+        }
+        else{
+            System.out.println(original+"  a palindrome number");
+        }
+    }
+}
