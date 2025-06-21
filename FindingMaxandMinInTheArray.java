f2f72335d56ac23cf50aa433f84690e34a633090
NIRANJANI <niranjniranjani4@gmail.com>
2025-06-21 22:52:37 +0530

    Add files via upload

---
+++ FindingMaxandMinInTheArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FindingMaxandMinInTheArray{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int userInput = scan.nextInt();
+        
+        int[] arr = new int[userInput];
+        System.out.print("Enter the elements: ");
+        
+        for(int i=0;i<userInput;i++){
+            arr[i]=scan.nextInt();
+        }
+        int max = arr[0];
+        int min = arr[0];
+
+        for(int i=0;i<userInput;i++){
+            max = Integer.max(max, arr[i]);
+            min = Integer.min(min, arr[i]);
+        }
+        System.out.println("Maximum Element is "+max);
+        System.out.println("Minimum Element is "+min);
+    }
+}
