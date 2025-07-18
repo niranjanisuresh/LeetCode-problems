dd560e230e3136adda2a1274efd27c96c72126a7
NIRANJANI <niranjniranjani4@gmail.com>
2025-07-18 13:11:09 +0530

    Add files via upload

--- PrintallDivisors.java
+++ PrintallDivisors.java
@@ -1,13 +1,13 @@
 public class PrintallDivisors {
-    public static void printDivisors(int n) {
+    public static void printDivisors(long n) {
         for(int i=1;i<=n;i++){
             if(n%i==0){
-                System.out.println(i);
+                System.out.print(i+" ");
             }
         }
     }
     public static void main(String[] args) {
-        int n = 36; // Example number
+        long n = 8; // Example number
         printDivisors(n);
     }
 }
